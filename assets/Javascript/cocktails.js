fecc0da86c88916717b86a08e72f1fc9f296e6f1
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-11-22 21:06:30 -0500

    updated JV
    Java script code for api

--- assets/Javascript/cocktails.js
+++ assets/Javascript/cocktails.js
@@ -4,23 +4,37 @@ var requestUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${use
 function getApi() {
     let requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + userInput;
     fetch(requestUrl)
-   
+
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            
-            for (var i = 0; i < data.length; i++) {
-                console.log(data.drinks[0].strDrink)
-                var listItem = document.createElement('li');
-                listItem.textContent = data[i].html_url;
-                repoList.appendChild(listItem);
-            }
-        });
+
+            var drinksArray = [data.drinks[0], data.drinks[1], data.drinks[2], data.drinks[3], data.drinks[4]]
+            for (var i = 0; i < drinksArray.length; i++) {
+                console.log(drinksArray)
+                var drinkTitle = drinksArray[i].strDrink;
+                // var drinkPic = drinksArray[i].strDrinkThumb;
+                var drinkIng = drinksArray[i].strIngredient + j;
+                var drinkIns = drinksArray[i].strInstructions
+                var div = document.createElement('div');
+                var drinkTitle = document.createElement('li');
+                var drinkPic = document.createElement('li');
+                var liRecipeId = document.createElement('li');
+                liRecipeTitle.textContent = recipeTitle
+                liRecipePic.textContent = recipePic
+                liRecipeId.textContent = recipeId
+                div.append(liRecipeTitle, liRecipePic, liRecipeId)
+                searchResultContainer.append(div)
+                
+                for (let index = 0; index < array.length; index++) {
+                    const element = array[index];
+                    
+                }}
+            });
 }
 getApi();
-
 
 
 
